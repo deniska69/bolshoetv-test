1bf21eb7951420f9e6077d7949a8bb305a863756
deniska69 <gostev.dy@yandex.ru>
2022-07-26 03:12:06 +0700

    Update layout

--- client/src/components/pages/movie/content/Content.jsx
+++ client/src/components/pages/movie/content/Content.jsx
@@ -5,7 +5,7 @@ import classes from './Content.module.css';
 const Content = ({movie}) => {
 
     return(
-        <div className={classes.Movie}>
+        <div className={classes.Content}>
             <div className={classes.Title}>
                 {movie.title}
             </div>
