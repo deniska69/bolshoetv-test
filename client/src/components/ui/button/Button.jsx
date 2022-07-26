648de5b8241f79f0e9f162389c1406720ea5be29
deniska69 <gostev.dy@yandex.ru>
2022-07-27 04:47:02 +0700

    Developed nev variant Button - Back

--- client/src/components/ui/button/Button.jsx
+++ client/src/components/ui/button/Button.jsx
@@ -2,12 +2,11 @@ import classes from './Button.module.css';
 import imageAppStore from '../../../assets/btn_app_appstore.png';
 import imageGooglePlay from '../../../assets/btn_app_googleplay.png';
 
-const Button = ({ children, className, onClick, campaign_id, content_id }) => {
+const Button = ({ className, onClick, campaign_id, content_id }) => {
 
   if(className === 'AppStore' || className === 'GooglePlay'){
     return (
       <a className={className === 'AppStore' ? classes.AppStore : classes.GooglePlay} 
-         onClick={onClick} 
          href={`https://bolshoe.app.link/WujiUvQIMrb?campaign_id=${campaign_id}&content_id=${content_id}`}
          target='_blank'
          rel='noreferrer'>
@@ -17,6 +16,14 @@ const Button = ({ children, className, onClick, campaign_id, content_id }) => {
       </a>
     );
   };
+
+  if(className === 'Back'){
+    return(
+      <button className={classes.Back} onClick={onClick}>
+        {`Показать все подборки`}
+      </button>
+    )
+  }
 };
 
 export default Button;
